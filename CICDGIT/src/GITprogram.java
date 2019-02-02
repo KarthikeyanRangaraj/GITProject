16220f96554f0275bb0171e7df2f476323acabc6
user <user@192.168.1.15>
2019-02-02 11:31:04 +0530

    2

--- CICDGIT/src/GITprogram.java
+++ CICDGIT/src/GITprogram.java
@@ -4,7 +4,7 @@ public class GITprogram {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		System.out.println("GIT Project");
+		System.out.println("GIT Project2");
 
 	}
 
